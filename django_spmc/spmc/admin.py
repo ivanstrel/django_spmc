5f0404c617212109c889dd39519d3805b3b043f5
Ivan <ivanstrel87@gmail.com>
2023-06-10 01:50:20 +0300

    Initial handdling of scene upload

--- django_spmc/spmc/admin.py
+++ django_spmc/spmc/admin.py
@@ -1,6 +1,70 @@
-from django.contrib import admin
+import os
+import subprocess
 
-from .models import Project, Scene
+from django import forms
+from django.conf import settings
+from django.contrib.gis import admin
+from django.contrib.gis.gdal import GDALRaster
+from django.core.checks import Error
+from django.core.files.temp import NamedTemporaryFile
+
+from .models import MiscTiles, Project, ProjectAlgos, Scene, SuperPixelAlgo
 
 admin.site.register(Project)
-admin.site.register(Scene)
+admin.site.register(SuperPixelAlgo)
+admin.site.register(ProjectAlgos)
+admin.site.register(MiscTiles)
+
+
+# =====================================================================================================================
+# Processing Scene with base tiles
+# =====================================================================================================================
+class SceneFormAdmin(forms.ModelForm):
+    image_file = forms.FileField(label="Spatial Raster Image")
+
+    class Meta:
+        model = Scene
+        fields = ["proj_id", "name", "description"]
+
+
+@admin.register(Scene)
+class SceneAdmin(admin.ModelAdmin):
+    readonly_fields = ["tiles_path", "bbox"]
+    form = SceneFormAdmin
+
+    def save_model(self, request, obj, form, change):
+        form = SceneFormAdmin(request.POST, request.FILES)
+        if not form.is_valid():
+            raise Error("Form is not valid")
+
+        clean_data = form.cleaned_data
+        obj.proj_id = clean_data["proj_id"]
+        obj.name = clean_data["name"]
+        obj.description = clean_data["description"]
+
+        # process image ===============================================================================================
+        image_file = request.FILES["image_file"]
+        # Generate random folder name
+        tmp_file = NamedTemporaryFile()
+        tmp_file_name = os.path.basename(tmp_file.name)
+        # Save file to temp
+        tmp_file.write(image_file.read())
+
+        # Define the output directory name folder based on the primary key of the Scene model
+        output_dir = f"{settings.MEDIA_ROOT}/tiles/{tmp_file_name}"
+        # TODO here is a problem, as new dir will be created for each change,
+        # TODO Also it is possible that the dir with such name already exists (not sure in random name generator)
+        # Check if output path exists
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+
+        # Run gdal2tiles.py as a subprocess, passing in the input and output paths
+        subprocess.run(["gdal2tiles.py", "-z", "10-18", "-w", "none", "-r", "bilinear", tmp_file.name, output_dir])
+        # Assign the output directory path to the Scene model
+        obj.tiles_path = output_dir
+        # Bounding box calculation ====================================================================================
+        rs = GDALRaster(tmp_file.name)
+        print(rs.srid)  # Dummy for pre_commit pass
+        # TODO add bounding box calculation
+        super().save_model(request, obj, form, change)
+        self.message_user(request, f"Tiles for {obj} have been processed and saved to {output_dir}")
--- django_spmc/spmc/models.py
+++ django_spmc/spmc/models.py
@@ -1,4 +1,7 @@
-from django.db import models
+import os
+import shutil
+
+from django.contrib.gis.db import models
 from django.utils.translation import gettext_lazy as _
 
 
@@ -9,9 +12,34 @@ class Project(models.Model):
 
     name = models.CharField(_("Project name"), max_length=255, unique=True, blank=False)
     description = models.TextField(_("Project description"), blank=True)
+    srid = models.IntegerField(default=3857)  # Default Spatial Reference ID of a project
 
     def __str__(self):
         return f"Project: {self.name}"
+
+
+class SuperPixelAlgo(models.Model):
+    """
+    SuperPixelType model for Django_SPMC
+    """
+
+    name = models.CharField(_("SuperPixelType name"), max_length=255, unique=True, blank=False)
+    description = models.TextField(_("SuperPixelType description"), blank=True)
+
+    def __str__(self):
+        return f"SP algorithm: {self.name}"
+
+
+class ProjectAlgos(models.Model):
+    """
+    List of approved Superpixel algorithms for each project
+    """
+
+    proj_id = models.ForeignKey(Project, on_delete=models.CASCADE)
+    algo_id = models.ForeignKey(SuperPixelAlgo, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Proj-Algo pair: {self.proj_id} - {self.algo_id}"
 
 
 class Scene(models.Model):
@@ -21,7 +49,50 @@ class Scene(models.Model):
 
     proj_id = models.ForeignKey(Project, on_delete=models.CASCADE)
     name = models.CharField(_("Scene name"), max_length=255, unique=True, blank=False)
-    description = models.TextField(_("Scene description"), blank=True)
+    description = models.TextField(_("Scene description"), blank=True, null=True)
+    tiles_path = models.FilePathField(blank=True, null=True, editable=False)
+    bbox = models.PolygonField(blank=True, null=True, srid=4326, editable=False)
 
     def __str__(self):
         return f"Scene: {self.name}"
+
+    def delete(self, *args, **kwargs):
+        # Delete the folder associated with this object
+        if self.tiles_path:
+            if os.path.exists(self.tiles_path):
+                shutil.rmtree(self.tiles_path)
+        super().delete(*args, **kwargs)
+
+
+class MiscTiles(models.Model):
+    """
+    MiscTiles model for Django_SPMC for storing paths and meta info about additional layers
+    """
+
+    scene_id = models.ForeignKey(Scene, on_delete=models.CASCADE)
+    name = models.CharField(_("MiscTiles name"), max_length=255, unique=True, blank=False)
+    description = models.TextField(_("MiscTiles description"), blank=True)
+    tiles_path = models.FilePathField(blank=True)
+    bbox = models.PolygonField(blank=True, null=True, srid=4326)
+
+    def __str__(self):
+        return f"MiscTiles: {self.name}"
+
+    def delete(self, *args, **kwargs):
+        # Delete the folder associated with this object
+        if os.path.exists(self.tiles_path):
+            shutil.rmtree(self.tiles_path)
+        super().delete(*args, **kwargs)
+
+
+class SuperPixel(models.Model):
+    """
+    SuperPixel model for Django_SPMC
+    """
+
+    scene_id = models.ForeignKey(Scene, on_delete=models.CASCADE)
+    sp = models.PolygonField(blank=True, null=True, srid=4326)
+    algo_id = models.ForeignKey(SuperPixelAlgo, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"SuperPixel: {self.id}, scene: {self.scene_id}"
